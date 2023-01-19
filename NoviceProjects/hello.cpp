d8f8c639929154866afedad1e054c2d0476061be
Zaki Rucker <zakirucker@mac.com>
2023-01-19 19:04:13 +0100

    reformatting

---
+++ NoviceProjects/hello.cpp
@@ -0,0 +1,19 @@
+//
+//  hello.cpp
+//  NoviceProjects
+//
+//  Created by Zaki Rucker on 1/18/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    if ( argc >= 2 ) {
+        cout << "Hello, " << argv[1] << "!" << endl;
+    } else {
+        cout << "Hello, World!" << endl;
+    }
+    return 0;
+}
