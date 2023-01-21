04135875695d2a56765396f0abaa9a914f009ae5
Zaki Rucker <zakirucker@mac.com>
2023-01-21 15:41:26 +0100

    user defined stopping point

--- NoviceProjects/fibonacci.cpp
+++ NoviceProjects/fibonacci.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
-using namespace std
+#include <string>
+using namespace std;
 
 int main (int argc, char * argv []) {
-    cout << "test" << endl;
+    int userInput = stoi(argv[1]);
+    int lowNum = 0;
+    int highNum = 1;
+    int newNum = 0;
+    while (newNum < userInput) {
+        newNum = lowNum + highNum;
+        lowNum = highNum;
+        highNum = newNum;
+        cout << newNum << endl;
+    }
     return 0;
 }
