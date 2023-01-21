d738476d1d8bec8fb8956fa61081e27a95f4135e
Zaki Rucker <zakirucker@mac.com>
2023-01-21 14:52:01 +0100

    Fibonacci created

---
+++ NoviceProjects/fibonacci.cpp
@@ -0,0 +1,14 @@
+//
+//  fibonacci.cpp
+//  
+//
+//  Created by Zaki Rucker on 1/21/23.
+//
+
+#include iostream
+using namespace std
+
+int main (int argc, char * argv []) {
+    cout << "test" << endl;
+    return 0;
+}
